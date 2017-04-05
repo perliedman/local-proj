d1e9d2fa0517e375425cbf89e3e4fb62f7c7e4cb
Per Liedman <per@liedman.net>
2017-04-05 20:53:31 +0200

    Add tests; update to latest proj4

---
+++ test/specs.js
@@ -0,0 +1,36 @@
+var test = require('tape'),
+	localProj = require('../');
+
+test('transforms origin correctly', function(t) {
+	var p = [11.9, 57.7];
+	var proj = localProj.find({type: 'Point', coordinates: p});
+
+	var projectedP = proj.forward(p);
+
+	t.equal(projectedP[0], 0);
+	t.equal(projectedP[1], 0);
+
+	t.end();
+});
+
+test('transforms distant point correctly', function(t) {
+	var point = {
+	  "type": "Feature",
+	  "properties": {
+	    "marker-color": "#0f0"
+	  },
+	  "geometry": {
+	    "type": "Point",
+	    "coordinates": [-75.343, 39.984]
+	  }
+	};
+	var proj = localProj.find(point);
+
+	// This point is calculated by Turf.js' destination
+	// function to be 50 miles from the origin
+	var projectedP = proj.forward([-74.39964, 39.98017]);
+	var distance = Math.sqrt(projectedP[0]*projectedP[0] + projectedP[1]*projectedP[1]);
+
+	t.ok(Math.abs(1 - distance / (50 * 1609.344)), 0.001);
+	t.end()
+});
