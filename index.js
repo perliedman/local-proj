b7ceb71f2d0a937b94574aaa2096a39819d33b65
Per Liedman <per@liedman.net>
2017-04-05 20:54:04 +0200

    Return proj4 instance according the docs

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = {
             }),
             clat = cs.sum[1] / cs.nCoords,
             clon = cs.sum[0] / cs.nCoords,
-            proj = proj4.Proj('+proj=tmerc +lat_0=' + clat + ' +lon_0=' + clon + ' +k=1.000000 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs');
+            proj = proj4('+proj=tmerc +lat_0=' + clat + ' +lon_0=' + clon + ' +k=1.000000 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs');
 
         return proj;
     }
